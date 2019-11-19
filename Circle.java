9f3120af33a5c0d542e27dd66d44c79a988d8069
aston-pm <57911480+aston-pm@users.noreply.github.com>
2019-11-19 09:32:24 -0600

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,20 @@
+package numberOne;
+
+public class Circle {
+
+    private int diameter;
+
+    public Circle(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+
+}
---
+++ Square.java
@@ -0,0 +1,18 @@
+package numberOne;
+
+public class Square {
+
+    private int lenwid;
+
+    public Square(int lenwid) {
+        this.lenwid = lenwid;
+    }
+
+    public void setLenwid(int lenwid) {
+        this.lenwid = lenwid;
+    }
+
+    public int getLenwid() {
+        return lenwid;
+    }
+}
